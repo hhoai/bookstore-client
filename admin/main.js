db57c2f9804b833e1de7c7676d46eefe76962ab3
hhoai <hhoaii261@gmail.com>
2023-05-11 16:15:24 +0700

    dashboard

--- admin/main.js
+++ admin/main.js
@@ -4,7 +4,8 @@ const sidebarElement = document.querySelectorAll(".sidebar-items");
 const subNav = document.querySelectorAll(".sub-nav");
 const subNavList = document.querySelector(".list");
 const category = document.querySelector(".category");
-
+const navInvoices = document.querySelector(".fa-ellipsis-h");
+const navInvoicesList = document.querySelector(".invoices-nav-container");
 
 
 toggleShow = function (ele,className) {
@@ -31,4 +32,10 @@ sidebarElement[1].onclick = function (e) {
 subNavList.onclick = function (e) {
   toggleShow(category, "hide");
   toggleShow(subNavList,"color");
+}
+
+navInvoices.onclick = function (e) {
+  toggleShow(navInvoices, "color");
+  toggleShow(navInvoicesList, "hide");
+  console.log("nav invoices clicked");
 }
